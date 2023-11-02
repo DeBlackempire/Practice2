4ec8aff74e26aa11961f074f19d0378d3bcc72f7
Eke-Abasi Isangifi <149294283+DeBlackempire@users.noreply.github.com>
2023-11-03 02:12:00 +1300

    Add files via upload

---
+++ tennis players.py
@@ -0,0 +1,51 @@
+print('List of 5 World best Tennis Player')
+print()
+
+PlayerName = 'Novak Djokovic'
+Age = 34
+Rank = 1
+
+Player_Name = 'Daniil Medvedev'
+Age1 = 25
+Rank1 = 2
+
+PlayerName1 = 'Alexander Zverev'
+Age2 = 24
+Rank2 = 3
+
+PlayerName2 = 'Stefanos Tsitsipas'
+Age3 = 23
+Rank3 = 4
+
+PlayerName3 = 'Andrey Rublev'
+Age4 = 24
+Rank4 = 5
+
+print('Player Name:',PlayerName)
+print('Rank:',Rank)
+print('Age:',Age)
+print()
+
+print('Player Name:',Player_Name)
+print('Rank:',Rank1)
+print('Age:',Age1)
+print()
+
+print('Player Name:',PlayerName1)
+print('Rank:',Rank2)
+print('Age:',Age2)
+print()
+
+print('Player Name:',PlayerName2)
+print('Rank:',Rank3)
+print('Age:',Age3)
+print()
+
+print('Player Name:',PlayerName3)
+print('Rank:',Rank4)
+print("Age:",Age4)
+print()
+
+print('Total Age Of All Players')
+a = (Age + Age1 + Age2 + Age3 + Age4)
+print(a)
